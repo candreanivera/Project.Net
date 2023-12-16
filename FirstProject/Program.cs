982d6f609bfcb43f07a93337d338dda987fd8829
Cristina Andreani <candreanivera@gmail.com>
2023-12-15 19:58:25 -0800

    First program.cs

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+string response;
+Console.WriteLine("What's your name?");
+//Captures an answer from the keyboard
+response = Console.ReadLine();
+Console.WriteLine($"Enjoy the course, {response}");
+
+//Displays information about the computer's SO 
+OperatingSystem thisOS = Environment.OSVersion;
+Console.WriteLine(thisOS.Platform);
+Console.WriteLine(thisOS.VersionString);
